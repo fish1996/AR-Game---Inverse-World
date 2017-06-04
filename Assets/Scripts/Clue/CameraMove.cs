0da44fc361873644a6f16447bccab9b6cd5c30ad
Dew-M <598271541@qq.com>
2017-06-04 19:55:00 +0800

    添加线索界面左右移动功能

---
+++ Assets/Scripts/Clue/CameraMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraMove : MonoBehaviour {
+
+    public Camera camera;
+    public Button LeftButton;
+    public Button RightButton;
+
+	// Use this for initialization
+	void Start () {
+        //LeftButton.on
+        LeftButton.onClick.AddListener(delegate()
+        {
+            LeftMove();
+        });
+        RightButton.onClick.AddListener(delegate()
+        {
+            RightMove();
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LeftMove()
+    {
+        Vector3 nowp = camera.transform.position;
+        if (nowp.x > -1719785)
+            camera.transform.Translate(new Vector3(-100, 0, 0));
+    }
+
+    void RightMove()
+    {
+        Vector3 nowp = camera.transform.position;
+        if (nowp.x < 1293211)
+            camera.transform.Translate(new Vector3(100, 0, 0));
+    }
+}
